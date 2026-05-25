600133792637dc5175e55d6a9f9676c3d3d5b41c
Wehnes <wenshao9799@gmail.com>
2026-05-25 10:07:44 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,3 @@
+$done({
+  content: "anytls=example.com:443, password=test, over-tls=true, tls-host=example.com, tls-verification=false, tls-alpn=02683208687474702f312e31, tls-no-session-ticket=true, udp-relay=true, tag=PARSER_TEST"
+});
